6ab79e529e3a3bb73a5d2eea614d7962517c81c6
TheWavesgod <xtr2811683@gmail.com>
2025-11-16 22:54:28 +0000

    update

--- Source/Vulkan/RenderPass.cpp
+++ Source/Vulkan/RenderPass.cpp
@@ -37,4 +37,129 @@ namespace VK
             outStream << std::format("[ renderPass ] ERROR\nFailed to create a render pass!\nError code: {}\n", int32_t(result));
         return result;
     }
+
+    RenderPass::Builder& RenderPass::Builder::AddAttachment(
+        VkFormat format, 
+        VkSampleCountFlagBits samples, 
+        VkAttachmentLoadOp loadOp, 
+        VkAttachmentStoreOp storeOp, 
+        VkAttachmentLoadOp stencilLoadOp, 
+        VkAttachmentStoreOp stencilStoreOp, 
+        VkImageLayout initialLayout, 
+        VkImageLayout finalLayout, 
+        VkAttachmentDescriptionFlags flags)
+    {
+		VkAttachmentDescription desc{};
+		desc.flags = flags;
+		desc.format = format;
+		desc.samples = samples;
+		desc.loadOp = loadOp;
+		desc.storeOp = storeOp;
+		desc.stencilLoadOp = stencilLoadOp;
+		desc.stencilStoreOp = stencilStoreOp;
+		desc.initialLayout = initialLayout;
+		desc.finalLayout = finalLayout;
+		attachments_.push_back(desc);
+		return *this;
+    }
+
+    RenderPass::Builder& RenderPass::Builder::AddColorAttachment(
+        VkFormat format, VkSampleCountFlagBits samples, VkImageLayout finalLayout)
+    {
+		return AddAttachment(
+			format,
+			samples,
+			VK_ATTACHMENT_LOAD_OP_CLEAR,
+			VK_ATTACHMENT_STORE_OP_STORE,
+			VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+			VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			VK_IMAGE_LAYOUT_UNDEFINED,
+			finalLayout
+		);
+    }
+
+    RenderPass::Builder& RenderPass::Builder::AddDepthStencilAttachment(
+        VkFormat format, VkSampleCountFlagBits samples, VkImageLayout finalLayout)
+    {
+		return AddAttachment(
+			format,
+			samples,
+			VK_ATTACHMENT_LOAD_OP_CLEAR,
+			VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			VK_ATTACHMENT_LOAD_OP_CLEAR,
+			VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			VK_IMAGE_LAYOUT_UNDEFINED,
+			finalLayout
+		);
+    }
+
+    RenderPass::Builder& RenderPass::Builder::BeginSubpass(VkPipelineBindPoint bindPoint)
+    {
+        SubpassConfig config{};
+        config.bindPoint = bindPoint;
+        subpasses_.push_back(config);
+        return *this;
+    }
+
+    RenderPass::Builder& RenderPass::Builder::AddColorAttachmentRef(uint32_t attachmentIndex, VkImageLayout layout)
+    {
+        VkAttachmentReference ref{};
+        ref.attachment = attachmentIndex;
+        ref.layout = layout;
+        subpasses_.back().colorAttachments.push_back(ref);
+    }
+
+    RenderPass::Builder& RenderPass::Builder::SetDepthStencilAttachmentRef(uint32_t attachmentIndex, VkImageLayout layout)
+    {
+        subpasses_.back().depthStencilAttachment = { .attachment = attachmentIndex, .layout = layout };
+        return *this;
+    }
+
+    RenderPass::Builder& RenderPass::Builder::AddDependency(
+        uint32_t srcSubpass, uint32_t dstSubpass, 
+        VkPipelineStageFlags srcStageMask, VkPipelineStageFlags dstStageMask, 
+        VkAccessFlags srcAccessMask, VkAccessFlags dstAccessMask, 
+        VkDependencyFlags dependencyFlags)
+    {
+        VkSubpassDependency dep{};
+		dep.srcSubpass = srcSubpass;
+		dep.dstSubpass = dstSubpass;
+		dep.srcStageMask = srcStageMask;
+		dep.dstStageMask = dstStageMask;
+		dep.srcAccessMask = srcAccessMask;
+		dep.dstAccessMask = dstAccessMask;
+		dep.dependencyFlags = dependencyFlags;
+		dependencies_.push_back(dep);
+        return *this;
+    }
+
+    RenderPass RenderPass::Builder::Build()
+    {
+        std::vector<VkSubpassDescription> subpassDescs(subpasses_.size(), VkSubpassDescription{});
+        for (size_t i = 0; i < subpasses_.size(); ++i)
+        {
+            const auto& src = subpasses_[i];
+            auto& dst = subpassDescs[i];
+
+            dst.pipelineBindPoint = src.bindPoint;
+            dst.colorAttachmentCount = static_cast<uint32_t>(src.colorAttachments.size());
+            dst.pColorAttachments = src.colorAttachments.data();
+            dst.pDepthStencilAttachment = &src.depthStencilAttachment;
+        }
+
+		VkRenderPassCreateInfo info{};
+		info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		info.attachmentCount = static_cast<uint32_t>(attachments_.size());
+		info.pAttachments = attachments_.empty() ? nullptr : attachments_.data();
+		info.subpassCount = static_cast<uint32_t>(subpassDescs.size());
+		info.pSubpasses = subpassDescs.data();
+		info.dependencyCount = static_cast<uint32_t>(dependencies_.size());
+		info.pDependencies = dependencies_.empty() ? nullptr : dependencies_.data();
+
+        VkRenderPass renderPass = VK_NULL_HANDLE;
+        if (vkCreateRenderPass(device_, &info, nullptr, &renderPass) != VK_SUCCESS)
+            throw std::runtime_error("Failed to create render pass!");
+
+        return RenderPass(renderPass);
+    }
 }
--- Source/Vulkan/RenderPass.h
+++ Source/Vulkan/RenderPass.h
@@ -8,35 +8,99 @@ namespace VK
     {
         VkRenderPass handle = VK_NULL_HANDLE;
 
+	public:
+		RenderPass() = default;
+		RenderPass(VkRenderPass renderPass) : handle(renderPass) {}
+		RenderPass(VkRenderPassCreateInfo& createInfo) { Create(createInfo); }
+
+		RenderPass(RenderPass&& other) noexcept { MoveHandle; }
+		~RenderPass();
+
+		// Getter
+		DefineHandleTypeOperator;
+		DefineAddressFunction;
+
+		// Const Function
+		void CmdBegin(VkCommandBuffer commandBuffer, VkRenderPassBeginInfo& beginInfo, VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE) const;
+
+		void CmdBegin(VkCommandBuffer commandBuffer, VkFramebuffer framebuffer,
+			VkRect2D renderArea, arrayRef<const VkClearValue> clearValues = {},
+			VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE) const;
+
+
+		void CmdNext(VkCommandBuffer commandBuffer, VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE) const {
+			vkCmdNextSubpass(commandBuffer, subpassContents);
+		}
+
+		void CmdEnd(VkCommandBuffer commandBuffer) const {
+			vkCmdEndRenderPass(commandBuffer);
+		}
+
+		//Non-const Function
+		result_t Create(VkRenderPassCreateInfo& createInfo);
+
     public:
-        RenderPass() = default;
-        RenderPass(VkRenderPassCreateInfo& createInfo) { Create(createInfo); }
+        class Builder
+        {
+        public:
+			explicit Builder(VkDevice device) : device_(device) {}
 
-        RenderPass(RenderPass&& other) noexcept { MoveHandle; }
-        ~RenderPass();
+            Builder& AddAttachment(
+                VkFormat format,
+                VkSampleCountFlagBits samples,
+                VkAttachmentLoadOp loadOp,
+                VkAttachmentStoreOp storeOp,
+                VkAttachmentLoadOp stencilLoadOp,
+                VkAttachmentStoreOp stencilStoreOp,
+                VkImageLayout initialLayout,
+                VkImageLayout finalLayout,
+                VkAttachmentDescriptionFlags flags = 0);
 
-        // Getter
-        DefineHandleTypeOperator;
-        DefineAddressFunction;
+            Builder& AddColorAttachment(
+                VkFormat format,
+                VkSampleCountFlagBits samples = VK_SAMPLE_COUNT_1_BIT,
+                VkImageLayout finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
 
-        // Const Function
-        void CmdBegin(VkCommandBuffer commandBuffer, VkRenderPassBeginInfo& beginInfo, VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE) const;
+            Builder& AddDepthStencilAttachment(
+                VkFormat format,
+                VkSampleCountFlagBits samples = VK_SAMPLE_COUNT_1_BIT,
+                VkImageLayout finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
 
-        void CmdBegin(VkCommandBuffer commandBuffer, VkFramebuffer framebuffer, 
-            VkRect2D renderArea, arrayRef<const VkClearValue> clearValues = {}, 
-            VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE) const;
+            // Subpass build 
+            Builder& BeginSubpass(VkPipelineBindPoint bindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS);
 
+            Builder& AddColorAttachmentRef(
+                uint32_t attachmentIndex,
+                VkImageLayout layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
 
-        void CmdNext(VkCommandBuffer commandBuffer, VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE) const {
-            vkCmdNextSubpass(commandBuffer, subpassContents);
-        }
+            Builder& SetDepthStencilAttachmentRef(
+                uint32_t attachmentIndex,
+                VkImageLayout layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
 
-        void CmdEnd(VkCommandBuffer commandBuffer) const {
-            vkCmdEndRenderPass(commandBuffer);
-        }
+            // Dependency part
+            Builder& AddDependency(
+                uint32_t srcSubpass,
+                uint32_t dstSubpass,
+                VkPipelineStageFlags srcStageMask,
+                VkPipelineStageFlags dstStageMask,
+                VkAccessFlags srcAccessMask,
+                VkAccessFlags dstAccessMask,
+                VkDependencyFlags dependencyFlags = 0);
 
-        //Non-const Function
-        result_t Create(VkRenderPassCreateInfo& createInfo);
+            RenderPass Build();
+
+        private:
+			struct SubpassConfig 
+            {
+				VkPipelineBindPoint bindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+				std::vector<VkAttachmentReference> colorAttachments;
+				VkAttachmentReference depthStencilAttachment;
+			};
+
+            VkDevice device_;
+            std::vector<VkAttachmentDescription> attachments_;
+            std::vector<SubpassConfig> subpasses_;
+            std::vector<VkSubpassDependency> dependencies_;
+        };
     };
-
 }
